636782b99c4f66e46c9c9b225218818a81ae90f2
ChronicStim <support@chronicstimulation.com>
2021-01-27 10:29:54 -0500

    refactor: Changes based on Analyzer warnings/errors.

--- JSONKit.m
+++ JSONKit.m
@@ -1035,20 +1035,6 @@ static JKHashTableEntry *_JKDictionaryHashTableEntryForKey(JKDictionary *diction
   return((entryForKey != NULL) ? entryForKey->object : NULL);
 }
 
-- (void)getObjects:(id *)objects andKeys:(id *)keys
-{
-  NSParameterAssert((entry != NULL) && (count <= capacity));
-  NSUInteger atEntry = 0UL; NSUInteger arrayIdx = 0UL;
-  for(atEntry = 0UL; atEntry < capacity; atEntry++) {
-    if(JK_EXPECT_T(entry[atEntry].key != NULL)) {
-      NSCParameterAssert((entry[atEntry].object != NULL) && (arrayIdx < count));
-      if(JK_EXPECT_T(keys    != NULL)) { keys[arrayIdx]    = entry[atEntry].key;    }
-      if(JK_EXPECT_T(objects != NULL)) { objects[arrayIdx] = entry[atEntry].object; }
-      arrayIdx++;
-    }
-  }
-}
-
 - (NSUInteger)countByEnumeratingWithState:(NSFastEnumerationState *)state objects:(id *)stackbuf count:(NSUInteger)len
 {
   NSParameterAssert((state != NULL) && (stackbuf != NULL) && (len > 0UL) && (entry != NULL) && (count <= capacity));
